bef183ee2b1975a487a7019cbdca62de55d13f42
Jared E <77979791+AquaPhoenix101@users.noreply.github.com>
2025-03-14 10:31:30 -0500

    Particles

--- SkeletonTemplate/Assets/Scripts/Telekinesis/UnlockItem.cs
+++ SkeletonTemplate/Assets/Scripts/Telekinesis/UnlockItem.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class UnlockItem : MonoBehaviour
 {
+
+    public GameObject particleEffect;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             TelekinesisPower.Instance.UnlockPower();
             Destroy(gameObject); // Remove the item after collection
+
+            Instantiate(particleEffect,other.transform.position,Quaternion.identity);
         }
     }
 }
